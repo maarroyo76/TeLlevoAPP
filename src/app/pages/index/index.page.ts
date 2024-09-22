b31b9c348d1b584a612845a4b05c69c3517d0a8e
Renato Reinoso <re.reinoso@duocuc.cl>
2024-09-21 22:19:51 -0300

    integracion de pagina  principal

--- src/app/pages/index/index.page.ts
+++ src/app/pages/index/index.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-index',
@@ -6,10 +7,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.page.scss'],
 })
 export class IndexPage implements OnInit {
+  nombreUsuario: string;
+  tieneAuto: boolean = false;
+  destino: string = '';
+  costoPorPersona: number | null = null;
+  viajesDisponibles = [
+    { destino: 'Puente Alto ', costo: 2000 },
+    { destino: 'La Florida', costo: 1500 },
+    { destino: 'Estadio Nacional', costo: 1000 },
+  ];
+  viajeSeleccionado: any = null;
+  mostrarContenido: boolean = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private alertController: AlertController) {
+    this.nombreUsuario = 'Martin';
   }
 
+  ngOnInit() {
+    this.presentAlert();
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: '¿Tienes auto?',
+      message: 'Elige una opción:',
+      buttons: [
+        {
+          text: 'Sí',
+          handler: () => {
+            this.manejarRespuesta(true);
+          }
+        },
+        {
+          text: 'No',
+          handler: () => {
+            this.manejarRespuesta(false);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  manejarRespuesta(respuesta: boolean) {
+    this.tieneAuto = respuesta;
+    this.mostrarContenido = true;
+  }
+
+  registrarViaje() {
+    console.log(`Destino: ${this.destino}, Costo por Persona: ${this.costoPorPersona}`);
+  }
+
+  seleccionarViaje(viaje: any) {
+    this.viajeSeleccionado = viaje;
+  }
+
+  confirmarSeleccion() {
+    if (this.viajeSeleccionado) {
+      console.log(`Viaje confirmado: ${this.viajeSeleccionado.destino}, Costo: ${this.viajeSeleccionado.costo}`);
+    }
+  }
 }
