f30ffaf530a7cce1ac104b91785eeaacc9cafcf0
Martin Arroyo <ma.arroyoq@duocuc.cl>
2024-10-27 20:26:48 -0300

    Cambios de interfaz y uso al index

--- src/app/pages/index/index.page.ts
+++ src/app/pages/index/index.page.ts
@@ -5,53 +5,77 @@ import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { Trip } from 'src/app/models/trip';
 import { TripService } from 'src/app/services/trip.service';
+import { Storage } from '@ionic/storage-angular';
+import { ViewWillEnter } from '@ionic/angular';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.page.html',
   styleUrls: ['./index.page.scss'],
 })
-export class IndexPage implements OnInit {
+export class IndexPage implements OnInit, ViewWillEnter {
   user: User | null = null;
-  destino: string = '';
-  costoPorPersona: number | null = null;
-  matricula: string = '';
-  horario: string = '';
-  choferNombre: string = '';
-  asientosDisponibles: number | null = null;
-  metodoPago: string = ''; 
-  viajesRegistrados: Trip[] = [];
-  mostrarFormulario: boolean = false;
-  mostrarViajes: boolean = false;
+  metodoPago: string = '';
   viajeSeleccionado: Trip | null = null;
-  mostrarContenido: boolean = true;
+  segment: string = 'Viajes';
+  mostrarFormulario: boolean = false;
+  lastTripId: number = 0;
+  driverName: string = '';
+  trips: Trip[] = [];
+
+  trip: Trip = {
+    id: 0,
+    driverName: '',
+    destination: '',
+    pricePerPerson: 0,
+    totalPassengers: 0,
+    totalCapacity: 0,
+    licensePlate: '',
+    driverId: 0,
+    passengerIds: [],
+    horario: '',
+  };
 
   constructor(
     private alertController: AlertController,
     private toastController: ToastController,
     private loginService: LoginService,
     private router: Router,
-    private tripService: TripService
-  ) {
-    if (!this.loginService.isAuth()) {
-      this.router.navigate(['/log-in']);
-    }
+    private tripService: TripService,
+    private storage: Storage
+  ) {}
+
+  ngOnInit(): void {
+    this.ionViewWillEnter();
   }
 
-  ngOnInit() {
-    const user = this.loginService.getCurrentUser();
-    if (user) {
-      this.user = user;
-      console.log('Usuario actual:', this.user);
-      this.choferNombre = this.user.username; 
+  async ionViewWillEnter() {
+    await this.storage.create();
+    const isAuthenticated = await this.loginService.isAuth(); // Asegúrate de usar await
+
+    if (!isAuthenticated) {
+      this.router.navigate(['/log-in']);
+    } else {
+      this.user = await this.loginService.getCurrentUser();
+      await this.loadTrips();
     }
-    this.loadTrips();
   }
 
   async loadTrips() {
     this.tripService.getTrips().subscribe(
-      (trips: Trip[]) => {
-        this.viajesRegistrados = trips; 
+      async (trips: Trip[]) => {
+        this.trips = trips;
+
+        // Obtener el último ID de viaje
+        if (this.trips.length > 0) {
+          this.lastTripId = Math.max(...this.trips.map(trip => trip.id));
+        } else {
+          this.lastTripId = 0; // Si no hay viajes, iniciar desde 0
+        }
+
+        for (let trip of this.trips) {
+          trip.driverName = await this.getDriverName(trip.driverId);
+        }
       },
       (error) => {
         console.error('Error al cargar los viajes:', error);
@@ -59,50 +83,72 @@ export class IndexPage implements OnInit {
     );
   }
 
+  async getDriverName(id: number): Promise<string> {
+    const cachedDriverName = await this.storage.get(`driver_${id}`);
+    if (cachedDriverName) {
+      return cachedDriverName;
+    } else {
+      return new Promise((resolve) => {
+        this.loginService.getUserById(id).subscribe((user: User) => {
+          const fullName = `${user.name} ${user.lastname}`;
+          this.storage.set(`driver_${id}`, fullName);
+          resolve(fullName);
+        });
+      });
+    }
+  }
+
+  validateForm() {
+    return this.trip.destination &&
+      this.trip.pricePerPerson &&
+      this.trip.totalCapacity &&
+      this.trip.licensePlate &&
+      this.trip.horario;
+  }
+
   async registrarViaje() {
-    if (this.destino && this.costoPorPersona !== null && this.matricula && this.horario && this.choferNombre && this.asientosDisponibles !== null) {
+    if (this.validateForm()) {
       const nuevoViaje: Trip = {
-        destination: this.destino,
-        pricePerPerson: this.costoPorPersona,
-        licensePlate: this.matricula,
+        ...this.trip,
+        id: this.lastTripId + 1, // Asignar el nuevo ID
         totalPassengers: 0,
-        totalCapacity: this.asientosDisponibles,
-        driverId: this.choferNombre,
         passengerIds: [],
-        horario: this.horario,
+        driverId: this.user!.id,
+        driverName: `${this.user!.name} ${this.user!.lastname}`,
       };
 
-      this.viajesRegistrados.push(nuevoViaje);
-
-      this.tripService.addTrip(nuevoViaje).subscribe(() => {
-        this.viajesRegistrados.push(nuevoViaje);
-        this.showToastMessage('Viaje registrado exitosamente', 'success');
-        this.clear();
-        this.mostrarFormulario = false;
-        this.loadTrips(); 
-      });
-
+      this.tripService.addTrip(nuevoViaje).subscribe(
+        (response) => {
+          if (response) {
+            this.showToastMessage('Viaje registrado exitosamente', 'success');
+            this.clear();
+            this.mostrarFormulario = false;
+            this.loadTrips();
+          }
+        },
+        (error) => {
+          console.error('Error al registrar el viaje:', error);
+          this.showToastMessage('Error al registrar el viaje. Inténtalo de nuevo.', 'danger');
+        }
+      );
     } else {
-      const alert = await this.alertController.create({
-        header: 'Error',
-        message: 'Por favor, completa todos los campos: destino, costo por persona, matrícula, horario, nombre del chofer y asientos disponibles.',
-        buttons: ['OK'],
-      });
-
-      await alert.present();
-      await alert.onDidDismiss();
       this.showToastMessage('Error al registrar el viaje', 'danger');
     }
   }
 
   clear() {
-    this.destino = '';
-    this.costoPorPersona = null;
-    this.matricula = '';
-    this.horario = '';
-    this.choferNombre = ''; 
-    this.asientosDisponibles = null;
-    this.metodoPago = ''; 
+    this.trip = {
+      id: 0,
+      driverName: '',
+      destination: '',
+      pricePerPerson: 0,
+      totalPassengers: 0,
+      totalCapacity: 0,
+      licensePlate: '',
+      driverId: 0,
+      passengerIds: [],
+      horario: '',
+    };
   }
 
   async showToastMessage(message: string, color: string) {
@@ -115,14 +161,21 @@ export class IndexPage implements OnInit {
     toast.present();
   }
 
+  async presentAlert(header: string, message: string, buttons: any[] = ['OK']) {
+    const alert = await this.alertController.create({
+      header,
+      message,
+      buttons,
+    });
+    await alert.present();
+  }
+
   mostrarFormularioViaje() {
-    this.mostrarFormulario = true;
-    this.mostrarViajes = false;
+    this.segment = 'mostrarFormulario'; // Cambia segment aquí
   }
 
   mostrarViajesRegistrados() {
-    this.mostrarViajes = true;
-    this.mostrarFormulario = false;
+    this.segment = 'mostrarViajes'; // Cambia segment aquí
   }
 
   async registrarseEnViaje(viaje: Trip) {
@@ -133,55 +186,56 @@ export class IndexPage implements OnInit {
         header: 'Método de Pago',
         message: 'Selecciona un método de pago:',
         inputs: [
-          {
-            name: 'metodoPago',
-            type: 'radio',
-            label: 'Efectivo',
-            value: 'efectivo',
-            checked: true
-          },
-          {
-            name: 'metodoPago',
-            type: 'radio',
-            label: 'Transferencia',
-            value: 'transferencia'
-          },
+          { name: 'metodoPago', type: 'radio', label: 'Efectivo', value: 'efectivo', checked: true },
+          { name: 'metodoPago', type: 'radio', label: 'Transferencia', value: 'transferencia' },
         ],
         buttons: [
           {
-            text: 'Cancelar',
-            role: 'cancel',
+        text: 'Cancelar', role: 'cancel', handler: () => {
+          this.viajeSeleccionado = null;
+        }
           },
           {
-            text: 'Confirmar',
-            handler: (data) => {
-              this.metodoPago = data.metodoPago;
-
-              if (this.viajeSeleccionado) {
-                this.viajeSeleccionado.passengerIds.push(this.user?.username || '');
-                this.viajeSeleccionado.totalPassengers++; 
-
-                this.loadTrips(); 
-
-                this.showToastMessage(`Te has registrado exitosamente en el viaje usando ${this.metodoPago}`, 'success');
-              }
-            },
+        text: 'Confirmar',
+        handler: (data) => {
+          this.metodoPago = data;
+          this.tripService.addPassenger(this.viajeSeleccionado!.id, this.user!.id.toString() || '')
+            .subscribe((response) => {
+          if (response) {
+            this.loadTrips();
+            this.showToastMessage(`Registrado exitosamente en el viaje con ${this.metodoPago}`, 'success');
+          }
+            }, (error) => {
+          this.showToastMessage('No se pudo registrar. Intenta de nuevo.', 'danger');
+            });
+        },
           },
         ],
       });
       await alert.present();
     } else {
-      this.showToastMessage('No hay asientos disponibles para registrarse en este viaje.', 'danger');
+      this.showToastMessage('No hay asientos disponibles en este viaje.', 'danger');
     }
   }
 
   async verCapacidad(viaje: Trip) {
+    const cantidadPasajeros = viaje.passengerIds.length;
+    const mensajeParticipantes = cantidadPasajeros > 0 
+      ? `Participantes: ${cantidadPasajeros}` 
+      : 'No hay pasajeros registrados aún';
+
     const alert = await this.alertController.create({
       header: 'Capacidad del Viaje',
       message: `Asientos Disponibles: ${viaje.totalCapacity - viaje.totalPassengers}\n` +
-               (viaje.passengerIds.length > 0 ? 'Participantes:\n' + viaje.passengerIds.join(', ') + `\n\nNombre: ${this.user?.username}` : 'No hay participantes registrados en este viaje.'),
+              mensajeParticipantes,
       buttons: ['OK'],
     });
     await alert.present();
+  }
+
+  async logout() {
+    this.loginService.logOut();
+    this.user = null;
+    this.router.navigate(['/log-in']);
   }
 }
